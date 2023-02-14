c45afaf8f603991a1cab36c4e67c1a2f40da42df
Dominika Janisz <mikaaa366@gmail.com>
2023-02-14 23:01:20 +0100

    generate of list

--- js/script.js
+++ js/script.js
@@ -44,13 +44,13 @@ const titleClickHandler = function(event){
     targetArticle.classList.add('active'); 
 
   }
-  
+  /*
   const links = document.querySelectorAll('.titles a');
 
   for(let link of links){
     link.addEventListener('click', titleClickHandler);
   }
-  
+  */
 
 const optArticleSelector = '.post',
     optTitleSelector = '.post-title',
@@ -58,24 +58,39 @@ const optArticleSelector = '.post',
 
 function generateTitleLinks(){
 
-  /* remove contents of titleList */
-  const titleList = document.querySelector(optTitleListSelector);
-  function clearTitleList() {
-    titleList.innerHTML = '';
-  }
-  clearTitleList();
-  /* for each article */
+    /* remove contents of titleList */
+    const titleList = document.querySelector(optTitleListSelector);
+        function clearTitleList() {
+        titleList.innerHTML = '';
+    }
+    clearTitleList();
+    console.log('titleList', titleList);
+    /* for each article */
+    const articles = document.querySelectorAll('.post');
+    let html = '';
+    for (let article of articles){
+    
+        /* get the article id */
+        const articleId = article.getAttribute('id');
+        /* find the title element */
+        /* get the title from the title element */
+        const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+        /* create HTML of the link */
+        const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+        console.log(linkHTML);
+        /* insert link into titleList */
+        titleList.insertAdjacentHTML('beforeend', linkHTML);
+        html = html + linkHTML;
+    }
 
-    /* get the article id */
+    titleList.innerHTML = html;
+    console.log('articles', articles);
+    const links = document.querySelectorAll('.titles a');
 
-    /* find the title element */
-
-    /* get the title from the title element */
-
-    /* create HTML of the link */
-
-    /* insert link into titleList */
-
+    for(let link of links){
+      link.addEventListener('click', titleClickHandler);
+    }
+    
 }
 
 generateTitleLinks();
